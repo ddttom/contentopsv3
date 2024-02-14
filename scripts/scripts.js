4139e999fb17534caf7418193713c1f68dba67d3
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-14 15:56:39 -0500

    Add site configuration and external image initialization***
    ***Update buildBlock function in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   sampleRUM,
   buildBlock,
@@ -13,6 +14,18 @@ import {
   loadCSS,
 } from './aem.js';
 
+import {
+  initialize as initSiteConfig,
+} from './siteConfig.js';
+
+import {
+  initialize as initExternalImage,
+} from './externalImage.js';
+
+initSiteConfig();
+initExternalImage();
+
+const setDelayed = true; // do (true) or not do (false) final load.
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
@@ -25,7 +38,9 @@ function buildHeroBlock(main) {
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', {
+      elems: [picture, h1],
+    }));
     main.prepend(section);
   }
 }
@@ -64,7 +79,7 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  //buildAutoBlocks(main);
+  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
@@ -77,12 +92,8 @@ async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
   const main = doc.querySelector('main');
-  if (main) {   ;
-    const inner = document.createElement("inner");
-    const header = document.createElement("header");
-    main.prepend(inner);
-    inner.append(header);
-    decorateMain(doc.querySelector('inner'));
+  if (main) {
+    decorateMain(main);
     document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
   }
@@ -102,15 +113,19 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('inner');
+  const main = doc.querySelector('main');
   await loadBlocks(main);
 
-  const { hash } = window.location;
+  const {
+    hash,
+  } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  //loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+  if (!window.hlx.suppressFrame) {
+    loadHeader(doc.querySelector('header'));
+    loadFooter(doc.querySelector('footer'));
+  }
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
@@ -126,11 +141,20 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
+  if (setDelayed) {
+    window.setTimeout(() => import('./delayed.js'), 3000);
+  }
   // load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.get('suppressFrame')) {
+    window.hlx.suppressFrame = true;
+    document.body.querySelector('header').remove();
+    document.body.querySelector('footer').remove();
+  }
+
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
