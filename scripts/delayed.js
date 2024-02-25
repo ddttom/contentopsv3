ab597c0b2a1176c6d413eb9a50c53ba22f5fdbca
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-25 09:49:31 +0000

    Update Dante AI chatbot configuration

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,17 +5,6 @@ import { sampleRUM, loadScript } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-// window.danteEmbed = 'https://chat.dante-ai.com/embed?kb_id=62b025de-a6f4-4344-be91-22f673a1d232&token=595446f0-11af-4e81-9543-a08139e438f7&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=null&bubbleopen=false';
-// loadScript('https://chat.dante-ai.com/bubble-embed.js');
-// loadScript('https://chat.dante-ai.com/dante-embed.js');
-
-const firstH1 = document.querySelector('h1');
-if (firstH1) {
-  const appendString = '<iframe src="https://chat.dante-ai.com/embed?kb_id=62b025de-a6f4-4344-be91-22f673a1d232&token=cc88b682-8338-4756-a686-2c45fb92314b&modeltype=gpt-3.5-turbo&logo=ZmFsc2U=&mode=false" allow="clipboard-write; microphone" width="100%" height="950" frameborder="0" ></iframe>';
-
-  // Append the constructed string to the h1 element's current content
-  const newElement = document.createElement('div');
-  newElement.className = 'chatBot';
-  newElement.innerHTML = appendString;
-  firstH1.insertAdjacentElement('afterend', newElement);
-}
+window.danteEmbed = 'https://chat.dante-ai.com/embed?kb_id=62b025de-a6f4-4344-be91-22f673a1d232&token=1b695881-6bfa-4b1a-93d4-15fc73a0166e&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=https://dante-chatbot-pictures.s3.amazonaws.com/62b025de-a6f4-4344-be91-22f673a1d232/simpleA.png&bubbleopen=false';
+loadScript('https://chat.dante-ai.com/bubble-embed.js');
+loadScript('https://chat.dante-ai.com/dante-embed.js');
