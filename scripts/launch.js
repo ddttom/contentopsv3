4465932b58350c1427d86690aceb614a46e5c88f
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-03-01 07:36:42 +0000

    Refactor code to improve performance and readability

---
+++ scripts/launch.js
@@ -0,0 +1,7 @@
+// This is the last chance, things in here are not allowed to change the display of the DOM.
+
+// eslint-disable-next-line import/prefer-default-export
+export async function initialize() {
+
+// add your own scripts here
+}
