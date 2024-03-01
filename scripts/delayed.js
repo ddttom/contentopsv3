a7046f0d19421ec7e11e8c88471e613fa301021b
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-03-01 07:39:45 +0000

    Remove unused code and update delayed functionality

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,13 +1,10 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM, loadScript } from './aem.js';
-import { initialize as initLaunch } from './launch.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 // add more delayed functionality here
-if (window.cmsplus.analyticsdelay > 0) {
-  initLaunch();
-}
+
 if (window.cmsplus.bubbleallowed === true) {
   window.danteEmbed = `https://chat.dante-ai.com/embed?${window.cmsplus.bubble}&mode=false&bubble=true&image=null&bubbleopen=false`;
   loadScript('https://chat.dante-ai.com/bubble-embed.js');
