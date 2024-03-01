47c68e431ada29c38e623f8fdaf18a2118c8e84a
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-03-01 07:24:50 +0000

    Update Dante embed URL and remove unnecessary parameters

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,6 @@ import { sampleRUM, loadScript } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-window.danteEmbed = 'https://chat.dante-ai.com/embed?kb_id=62b025de-a6f4-4344-be91-22f673a1d232&token=1b695881-6bfa-4b1a-93d4-15fc73a0166e&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=https://dante-chatbot-pictures.s3.amazonaws.com/62b025de-a6f4-4344-be91-22f673a1d232/simpleA.png&bubbleopen=false';
+window.danteEmbed = `https://chat.dante-ai.com/embed?${window.cmsplus.bubble}&mode=false&bubble=true&image=null&bubbleopen=false`;
 loadScript('https://chat.dante-ai.com/bubble-embed.js');
 loadScript('https://chat.dante-ai.com/dante-embed.js');
